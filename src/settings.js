0606f8980e197ece9fe35b3beef4cbdf0fd0903d
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-30 14:14:58 -0600

    Read API from environmental variable. If undefined use an empty string

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,10 @@
 let API_URL;
+console.log(process.env.BACKEND_API_URL);
+alert(process.env.BACKEND_API_URL);
 if (process.env.BACKEND_API_URL) {
   API_URL = process.env.BACKEND_API_URL;
 } else {
-  API_URL = "";
+  API_URL = "http://localhost:8000";
 }
 
 export default API_URL;
